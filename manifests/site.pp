1a8da2c7fe2df56afdc6664792381baf7d4f771d
Barry Peddycord III <ishara@isharacomix.org>
2017-07-21 10:38:02 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,18 +7,18 @@ node default {
 
 # We use the hostname to figure out what roles we should be assigning to the
 # node. For leaves and spines, we can use a catch-all regular expression.
-node /^leaf\d+$/ {
+node /^leaf\d+.simulation$/ {
     include quagga
     include ifupdown2
 }
 
-node /^spine\d+$/ {
+node /^spine\d+.simulation$/ {
     include quagga
     include ifupdown2
 }
 
 
-node server02 {
+node server02.simulation {
     include ifupdown
     include apache
 }
